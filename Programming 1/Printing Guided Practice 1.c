87e9b70f1f335239de75e3c413f66d79ad1b8b4c
Matthew <78315885+Azurieum@users.noreply.github.com>
2024-03-11 22:50:43 -0400

    Create Printing Guided Practice 1.c
    Printing practice as well, followed instructions to gain familiarity with C and the complier.

---
+++ Programming 1/Printing Guided Practice 1.c
@@ -0,0 +1,34 @@
+//Heading
+//Name: Matt
+//Date: 9/15/23
+//Goal:  Guided Practice Printing #1
+//************************************
+
+#include<stdio.h>   //C library
+
+int main()
+{
+    //Slide #2
+    printf("Hello World");
+    printf("   Hello World\n");
+    printf("Hello\nWorld");
+    printf("Hello\n World\n");
+    
+    //Slide #5
+    printf("(\\_/) \n");
+    printf("( . .) \n");
+    printf("c(\”)(\”) \n");
+    
+    printf("(\\_/) \n");
+    printf("(o.o)  \n");
+    printf("(__)0 \n");
+    //Slide #7
+    printf("|\\---/|\n");
+    printf("| o_o |\n");
+    printf(" \\_^_/\n");
+    
+    printf(" /\\_/\\ \n");
+    printf("( o.o ) \n");
+    printf(" > ^ <");
+    return 0;
+}
