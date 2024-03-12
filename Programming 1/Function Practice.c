daa0759214629b30f492115373ffdbd0c56455eb
Matthew <78315885+Azurieum@users.noreply.github.com>
2024-03-11 23:03:51 -0400

    Create Function Practice.c
    Familiarity with functions and calling functions.

---
+++ Programming 1/Function Practice.c
@@ -0,0 +1,57 @@
+/******************************************************************************
+
+Functions 1:  Functions with no input or returned value
+The function stub has been included below.  Your job is to complete the stubs.
+Due: 11/16/2023 
+
+*******************************************************************************/
+#include <stdio.h>
+void YourFirstName(void)
+{   //print your first name in block letters across the page
+    //input: none
+    //returned value: none
+    printf(" #     #                                         \n");
+    printf(" ##   ##   ##   ##### ##### #    # ###### #    # \n");
+    printf(" # # # #  #  #    #     #   #    # #      #    # \n");
+    printf(" #  #  # #    #   #     #   ###### #####  #    # \n");
+    printf(" #     # ######   #     #   #    # #      # ## # \n");
+    printf(" #     # #    #   #     #   #    # #      ##  ## \n");
+    printf(" #     # #    #   #     #   #    # ###### #    # \n");
+    
+    
+    
+}
+void YourLastName(void)
+{   //print your last name in block letters across the page
+    //input: none
+    //returned value: none
+    
+    //removed due to privacy
+    
+    
+}
+void YourInitials(void)
+{   //print your initials in block letters across the page
+    //input: none
+    //returned value: none
+    
+    printf(" #     #    #     # \n");
+    printf(" ##   ##    ##   ## \n");
+    printf(" # # # #    # # # # \n");
+    printf(" #  #  #    #  #  # \n");
+    printf(" #     #    #     # \n");
+    printf(" #     #    #     # \n");
+    printf(" #     #    #     # \n");
+    
+}
+
+
+
+int main()
+{
+    YourFirstName();
+    YourLastName();
+    YourInitials();
+
+    return 0;
+}
